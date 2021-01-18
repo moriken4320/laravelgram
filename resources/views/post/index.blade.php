22c7a69d67fe1c60c68f598a1148812fe7321e83
moriken4320 <moriken.4320@gmail.com>
2021-01-18 23:10:57 +0900

    ナビゲーションヘッダーの作成

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,6 +1,15 @@
+@extends('layouts.app')
+
+@include('navbar')
+
+@section('content')
+    
+
 {{--  Bootstrap  --}}
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 <p>このページは仮のトップページです</p>
 
 <a href="#" class="btn btn-success">仮のボタンです</a>
+
+@endsection
