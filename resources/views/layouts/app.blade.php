5b4baa918d5b6aa9fd026c0e4de8304fef54f684
moriken4320 <moriken.4320@gmail.com>
2021-01-20 12:26:34 +0900

    cssファイルの読み込みを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,12 @@
     <!--Bootstrap CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Styles -->
+    @if(app('env')=='local')
     <link href="{{ asset('css/application.css') }}" rel="stylesheet">
+    @endif
+    @if(app('env')=='production')
+    <link href="{{ secure_asset('css/application.css') }}" rel="stylesheet">
+    @endif
 
     {{-- Jquery --}}
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
